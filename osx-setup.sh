9958b2a82eaba31f1f100bb735c675c7d90b5a94
david <lambdan@icloud.com>
2014-01-31 20:02:59 +0100

    Update osx-setup.sh

--- osx-setup.sh
+++ osx-setup.sh
@@ -1,3 +1,5 @@
+# don't recommend using it atm
+
 # install homebrew(brew)
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew install wget
@@ -6,6 +8,6 @@ brew install ssh-copy-id
 # download .vimrc and .bash_profile into ~
 wget -O ~/.vimrc https://raw.github.com/lambdan/config/master/.vimrc --no-check-certificate
 
-wget -O ~/.vimrc https://raw.github.com/lambdan/config/master/.bash_profile --no-check-certificate
+wget -O ~/.bash_profile https://raw.github.com/lambdan/config/master/.bash_profile --no-check-certificate
 
 echo "Everything should be done."
